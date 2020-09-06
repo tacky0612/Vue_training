ae55760c7c01c59903492ecb380996b95b92c7aa
tacky0612 <tacky.g25@gmail.com>
2020-09-07 00:11:44 +0900

    算出プロパティとウォッチャ

--- js/intro.js
+++ js/intro.js
@@ -1,5 +1,5 @@
-var app = new Vue({
-    el: '#app',
+var app1 = new Vue({
+    el: '#app-1',
     data: {
         message: 'Hello Vue!'
     }
@@ -77,3 +77,105 @@ var app7 = new Vue({
         ]
     }
 })
+
+var app = new Vue({
+    el: '#app',
+    data: {
+        rawHtml:"<button>test</button>",
+        isButtonDisabled:true,
+        number:10,
+        ok: true,
+        message: "message",
+        id: "123",
+        seen: false,
+        url:"/",
+        doSomething:function(){
+            console.log("aaa");
+        },
+        attributeName:function(url){
+            console.log(url);
+        }
+    }
+})
+
+var example = new Vue({
+    el:'#example',
+    data:{
+        message : 'Hello Vue'
+    },
+    computed:{
+        reversedMessage: function(){
+            return this.message.split('').reverse().join('')
+        }
+    },
+    methods:{
+        reverseMessage: function(){
+            return this.message.split('').reverse().join('')
+        }
+    }
+})
+
+
+var vm = new Vue({
+  el: '#demo',
+  data: {
+    firstName: 'Foo',
+    lastName: 'Bar'
+  },
+  computed: {
+    fullName: {
+        // getter 関数
+        get: function () {
+          return this.firstName + ' ' + this.lastName
+        },
+        // setter 関数
+        set: function (newValue) {
+          var names = newValue.split(' ')
+          this.firstName = names[0]
+          this.lastName = names[names.length - 1]
+        }
+      }
+  }
+})
+
+var watchExampleVM = new Vue({
+    el: '#watch-example',
+    data: {
+      question: '',
+      answer: 'I cannot give you an answer until you ask a question!'
+    },
+    watch: {
+      // この関数は question が変わるごとに実行されます。
+      question: function (newQuestion, oldQuestion) {
+        this.answer = 'Waiting for you to stop typing...'
+        this.debouncedGetAnswer()
+      }
+    },
+    created: function () {
+      // _.debounce は特にコストの高い処理の実行を制御するための
+      // lodash の関数です。この場合は、どのくらい頻繁に yesno.wtf/api
+      // へのアクセスすべきかを制限するために、ユーザーの入力が完全に
+      // 終わるのを待ってから ajax リクエストを実行しています。
+      // _.debounce (とその親戚である _.throttle )  についての詳細は
+      // https://lodash.com/docs#debounce を見てください。
+      this.debouncedGetAnswer = _.debounce(this.getAnswer, 500)
+    },
+    methods: {
+      getAnswer: function () {
+        if (this.question.indexOf('?') === -1) {
+          this.answer = 'Questions usually contain a question mark. ;-)'
+          return
+        }
+        this.answer = 'Thinking...'
+        var vm = this
+        axios.get('https://yesno.wtf/api')
+          .then(function (response) {
+            vm.answer = _.capitalize(response.data.answer)
+          })
+          .catch(function (error) {
+            vm.answer = 'Error! Could not reach the API. ' + error
+          })
+      }
+    }
+  })
+  
